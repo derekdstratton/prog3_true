76012e521fbeb5d32b1a70754414228c677a957c
Gunner Stone <gunnerinlv@gmail.com>
2021-04-21 15:21:53 -0700

    gunner eigen working

--- GunnerPlayground/ConsoleApplication1/ConsoleApplication1.cpp
+++ GunnerPlayground/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,16 +1,21 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <Eigen/Dense>
+#include <opencv2/core/eigen.hpp>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <vector>
 #include <filesystem>
 
-using namespace cv;
 using namespace std;
 
-// http://www.cplusplus.com/forum/windows/189681/
-//
+void view_vector_as_image(Eigen::VectorXd image_vec, int rows, int cols)
+{
+    Eigen::MatrixXd mat2 = image_vec.reshaped(rows, cols);
+    cv::Mat image2;
+    cv::eigen2cv(mat2, image2);
+    image2.convertTo(image2, CV_32FC1, 1.f / 255);
+    cv::imshow("pls", image2);
+    cv::waitKey(0);
+}
+
 std::vector<std::string> get_filenames(filesystem::path path)
 {
     std::vector<std::string> filenames;
@@ -29,123 +34,100 @@ std::vector<std::string> get_filenames(filesystem::path path)
 
 int main()
 {
-    vector<Mat> faces;
-    /// Reads in all hd faces and stores them unflattened in a vector
-    for (string name : get_filenames("Faces_FA_FB/fa_H"))
+    bool A = 0, B = 1, C = 0, D = 0, E = 0;
+    //Debug/Test Block
+    if (A)
     {
-        Mat im = imread(name,IMREAD_GRAYSCALE);
-        // normalize im to contain values between 0 - 1
-        normalize(im, im, 0.0, 1.0, NORM_MINMAX, CV_64FC1);
-        faces.push_back(im);
+        cv::Mat image = cv::imread("Faces_FA_FB/fa_H/00001_930831_fa_a.pgm", cv::IMREAD_GRAYSCALE);
+        Eigen::MatrixXd mat;
+        cv::cv2eigen(image, mat);
+        std::cout << "Original Image Dim" << mat.rows() << " " << mat.cols() << std::endl;
+        Eigen::VectorXd vec = mat.reshaped();
+        std::cout << "Flattened vector size " << vec.size() << std::endl;
+        view_vector_as_image(vec, mat.rows(), mat.cols());
     }
-    cout << "CP1" << endl;
-
-    /// Calculate the mean face of the dataset
-    Mat mean = Mat::zeros(Size(faces[0].cols, faces[0].rows), CV_64FC1);
-    for (Mat face : faces)
+    if (B)
     {
-        //store running total of face values
-        mean += face;
-    }
-    // divide by total number of faces to get mean
-    mean /= double(faces.size());
-
-    
-    //imshow("windowname", mean);
-    //waitKey(0);
-
-    cout << "CP2" << endl;
-
-    /// Subtract mean from every face to normalize them
-    for (int i = 0; i < faces.size(); i++)
-    {
-        faces.at(i) -= mean;
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////
-    // Create a matrix of flattened faces; call this matrix A
-    // faces.size() is the number of samples
-    // faces[0].cols * facces[0].rows gets the size of the flattened image vector
-    // Size() opencv datatype takes in (width,height) of matrix
-    ////////////////////////////////////////////////////////////////////////////////
-
-    Mat A = Mat::zeros(Size(faces.size(), faces[0].cols * faces[0].rows), CV_64FC1);
-    //cout << "A rows: " << A.rows << " A cols:" << A.cols << endl;
-
-    // for every face
-    for (int i = 0; i < faces.size(); i++)
-    {
-        Mat face = faces[i];
-        face = face.t();
-
-        // for every pixel
-        for (int j = 0; j < face.cols; j++)
+        auto file_name_list = get_filenames("Faces_FA_FB/fa_H");
+        cv::Mat first_sample = cv::imread("Faces_FA_FB/fa_H/00001_930831_fa_a.pgm", cv::IMREAD_GRAYSCALE);
+        int num_rows = first_sample.rows;
+        int num_cols = first_sample.cols;
+        int num_features = num_rows * num_cols;
+        int num_samples = file_name_list.size();
+        // Reads in all hd faces and stores them unflattened in a vector
+        //num_features x num_samples (NxM) matrix
+        Eigen::MatrixXd original_data = Eigen::MatrixXd(num_features, num_samples);
+        for (int i = 0; i < num_samples; i++)
         {
-            for (int k = 0; k < face.rows; k++)
-            {
-                // Mat.at() takes in (row,col) indexing
-                A.at<double>(k + j * face.rows, i) = (double)face.at<double>(k, j);
-            }
+            string name = file_name_list[i];
+            cv::Mat im = imread(name, cv::IMREAD_GRAYSCALE);
+            Eigen::MatrixXd mat;
+            cv::cv2eigen(im, mat);
+            original_data.col(i) = mat.reshaped();
         }
-    }
+        cout << "Original Data matrix: " << original_data.rows() << " " << original_data.cols() << endl;
+        //        view_vector_as_image(original_data.col(0), num_rows, num_cols);
 
-    // Sanity check to make sure faces weres flattened/stored in A correctly
-    // Unflatten and display the first face at column 0
-    /*
-    Mat testImg = Mat(faces[0].size(), CV_64FC1);
-    for (int i = 0; i < faces[0].rows; i++)
-    {
-        for (int j = 0; j < faces[0].cols; j++)
+                //Sample Mean
+                //step 1
+        Eigen::VectorXd sample_mean;
+        Eigen::VectorXd x_bar = Eigen::VectorXd::Zero(num_features);
+        for (int i = 0; i < num_samples; i++)
         {
-            testImg.at<double>(i, j) = A.at<double>(j + i * faces[0].cols, 0);
+            x_bar += original_data.col(i);
         }
-    }
-    imshow("Display window", testImg+mean);
-    waitKey(0);
-    */
+        x_bar /= num_samples;
+        //        view_vector_as_image(x_bar, num_rows, num_cols);
 
-    // .t() doesnt transpose the object that calls it, returns a transposed matrix
-    // https://docs.opencv.org/3.4/d3/d63/classcv_1_1Mat.html#aaa428c60ccb6d8ea5de18f63dfac8e11
-    Mat t = A.t();
-
-
-    Mat cov = t*A;
-
-
-    // Size of covarianace matrix should be MxM where M is number of training samples
-    cout << "Cov rows:" <<cov.rows << " Cov Cols:" << cov.cols << endl;
-
-    // Compute eigen values & eigen vectors of covariance matrix and stores in descending order
-    // https://docs.opencv.org/2.4/modules/core/doc/operations_on_arrays.html?highlight=eigen#bool%20eigen%28InputArray%20src,%20OutputArray%20eigenvalues,%20OutputArray%20eigenvectors,%20int%20lowindex,int%20highindex%29
-    Mat eigenValues, eigenVectors;
-    eigen(cov, eigenValues, eigenVectors);
-    cout << "Reconstructing Eigen Faces.." << endl;
-    Mat eigenFaces = A * eigenVectors;
-
-
-    // Sanity check to make sure eigenFaces arent garbage
-    // Unflatten and display the first eigenFace at column 0
-    /*
-    Mat testImg = Mat(faces[0].size(), CV_64FC1);
-    for (int i = 0; i < faces[0].rows; i++)
-    {
-        for (int j = 0; j < faces[0].cols; j++)
+                //step 2
+        Eigen::MatrixXd centered_data = Eigen::MatrixXd(num_features, num_samples);
+        for (int i = 0; i < num_samples; i++)
         {
-            testImg.at<double>(i, j) = eigenFaces.at<double>(j + i * faces[0].cols, 0);
+            centered_data.col(i) = original_data.col(i) - x_bar;
         }
-    }
-    imshow("Display window", testImg + mean);
-    waitKey(0);
-    */
+        //        view_vector_as_image(centered_data.col(0), num_rows, num_cols);
 
-    // Convert data back to scale of 0 - 255 so it can be read by an image editor like GIMP
-    cout << "Normalizing mean back to 0 - 255" << endl;
-    normalize(mean, mean, 0, 255, NORM_MINMAX, CV_8UC1);
-    cout << "Normalizing eigenfaces 0-255" << endl;
-    normalize(eigenFaces, eigenFaces, 0, 255, NORM_MINMAX, CV_8UC1);
-    // store as PGMs
-    imwrite("mean.pgm", mean);
-    imwrite("eigenfaces.pgm", eigenFaces);
+                //step 3 (MxM)
+        auto A_transpose_A = centered_data.transpose() * centered_data;
+        cout << "A Transpose A matrix: " << A_transpose_A.rows() << " " << A_transpose_A.cols() << endl;
+
+        //step 4 (takes like 5 min or so)
+        Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> s(A_transpose_A);
+        //they are sorted from SMALLEST TO LARGEST.ffs.
+        cout << "Eigenvals " << s.eigenvalues().reverse() << endl;
+        //4b
+        Eigen::MatrixXd reversed_eigenvectors = s.eigenvectors().rowwise().reverse();
+
+        cout << "Checkpoint" << endl;
+
+        //num_features x num_samples
+        //this multiplication also takes a hot minute
+        //todo: I'm not sure if it's supposed to be original_data or centered_data
+        Eigen::MatrixXd transformed_eigens = centered_data * reversed_eigenvectors;
+
+        cout << "transformed eigenfaces dims" << transformed_eigens.rows() << " " << transformed_eigens.cols() << endl;
+        //normalize to unit length
+        for (int i = 0; i < transformed_eigens.cols(); i++)
+        {
+            transformed_eigens.col(i) = transformed_eigens.col(i) / transformed_eigens.col(i).norm();
+        }
+
+        //trying it on image1
+        //1 x num_samples
+        //todo: original or centered
+        Eigen::MatrixXd y = centered_data.col(0).transpose() * transformed_eigens;
+        cout << "y dims" << y.rows() << " " << y.cols() << endl;
+
+        cout << y << endl;
+
+        Eigen::VectorXd x_hat = Eigen::VectorXd::Zero(num_features);
+        for (int k = 0; k < 1000; k++)
+        {
+            x_hat += y(0, k) * transformed_eigens.col(k);
+        }
+
+        view_vector_as_image(x_hat + x_bar, num_rows, num_cols);
+    }
 
     return 0;
 }
