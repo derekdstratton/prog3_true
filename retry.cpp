b9b54e458c22bc9942f33fb566c3fe33817bc532
Derek <derekdstratton@gmail.com>
2021-04-23 14:08:03 -0700

    overcooked

--- retry.cpp
+++ retry.cpp
@@ -6,8 +6,16 @@
 
 using namespace std;
 
-//todo: write: save_vector_as_image
-//write: load_images_from_path and load_identities
+//name should be .pgm
+void save_vector_as_image(Eigen::VectorXd image_vec, int rows, int cols, string name)
+{
+    Eigen::MatrixXd mat2 = image_vec.reshaped(rows, cols);
+    cv::Mat image2;
+    cv::eigen2cv(mat2, image2);
+    image2.convertTo(image2, CV_32FC1, 1.f*255);
+    cv::imwrite(name, image2);
+    cv::waitKey(0);
+}
 
 double mahalanobis(Eigen::VectorXd y1, Eigen::VectorXd y2, Eigen::VectorXd lambda)
 {
@@ -66,7 +74,7 @@ std::vector<std::string> get_filenames(filesystem::path path)
 
 int main()
 {
-    bool A = 0, B = 0, C = 0, D = 1, E = 0;
+    bool A = 0, B = 0, C = 0, D = 0, E = 0, F = 1;
     //Debug/Test Block
     if (A)
     {
@@ -299,6 +307,7 @@ int main()
             identities(i) = stoi(filesystem::path(name).filename().generic_string().substr(0,5));
         }
         cout << "Original Data matrix: " << original_data.rows() << " " << original_data.cols() << endl;
+//        save_vector_as_image(original_data.col(0), num_rows, num_cols, "first1.pgm");
 //        view_vector_as_image(original_data.col(0), num_rows, num_cols);
 
         //Sample Mean
@@ -328,7 +337,7 @@ int main()
         cout << "TOP K SIZE" << top_k_eigens.rows() << " " << top_k_eigens.cols() << endl;
 
         Eigen::MatrixXd training_data_projected = centered_data.transpose() * top_k_eigens;
-        cout << "Projected " << training_data_projected.rows() << " " << training_data_projected.cols() << endl;
+        cout << "Projected training data: " << training_data_projected.rows() << " " << training_data_projected.cols() << endl;
 
         Eigen::VectorXd top_k_eigenvals = eigenvalues(Eigen::seq(0, num_cmp_needed-1));
 
@@ -348,7 +357,305 @@ int main()
         cout << "IDENTITIES: " << identities(2) << ", " << identities(3) << endl;
 
         //Recognition: load in test imgs
+        auto test_names = get_filenames("Faces_FA_FB/fb_H");
+        int test_samples = test_names.size();
+        Eigen::MatrixXd test_data = Eigen::MatrixXd(num_features, test_samples);
+        Eigen::VectorXd test_identities = Eigen::VectorXd(test_samples);
+        for (int i = 0; i < test_samples; i++)
+        {
+            string name = test_names[i];
+            cv::Mat im = imread(name,cv::IMREAD_GRAYSCALE);
+            Eigen::MatrixXd mat;
+            cv::cv2eigen(im, mat);
+            mat /= 255.;
+            test_data.col(i) = mat.reshaped();
+//            cout << filesystem::path(name).filename().generic_string().substr(0,5) << endl;
+            test_identities(i) = stoi(filesystem::path(name).filename().generic_string().substr(0,5));
+        }
+        cout << "TEST DATA " << test_data.rows() << " " << test_data.cols() << endl;
+        Eigen::MatrixXd centered_test_data = Eigen::MatrixXd(num_features, test_samples);
+        for (int i = 0; i < test_samples; i++)
+        {
+            centered_test_data.col(i) = test_data.col(i) - x_bar;
+        }
+        Eigen::MatrixXd test_data_projected = centered_test_data.transpose() * top_k_eigens;
+        cout << "Projected test data: " << test_data_projected.rows() << " " << test_data_projected.cols() << endl;
 
+        //matrix of all mahalanobis dists
+        Eigen::MatrixXd training_test_difs(num_samples, test_samples);
+        int did_good = 0;
+        for (int i = 0; i < test_samples; i++)
+        {
+            for (int j = 0; j < num_samples; j++)
+            {
+                training_test_difs(j, i) = mahalanobis(training_data_projected.row(j),
+                                                       test_data_projected.row(i),
+                                                       top_k_eigenvals);
+            }
+            Eigen::VectorXd::Index index;
+            training_test_difs(Eigen::all, i).minCoeff(&index);
+//            cout << "Test Identity: " << test_identities(i) << ", Training Match: " << identities(index) << endl;
+            if (test_identities(i) == identities(index))
+            {
+                did_good++;
+            }
+        }
+        cout << "ACC: " << ((float)did_good) / ((float)test_samples) << endl;
+    }
+    //3b. take out the 1st 50 identities in training_set, compute new eigenspace, and do intruder detection
+    if (E)
+    {
+        auto file_name_list = get_filenames("Faces_FA_FB/fa_H");
+        cv::Mat first_sample = cv::imread("Faces_FA_FB/fa_H/00001_930831_fa_a.pgm", cv::IMREAD_GRAYSCALE);
+        int num_rows = first_sample.rows;
+        int num_cols = first_sample.cols;
+        int num_features = num_rows * num_cols;
+        int num_samples = file_name_list.size();
+        // Reads in all hd faces and stores them unflattened in a vector
+        //num_features x num_samples (NxM) matrix
+        Eigen::MatrixXd original_data = Eigen::MatrixXd(num_features, num_samples);
+        Eigen::VectorXd identities = Eigen::VectorXd(num_samples);
+        for (int i = 0; i < num_samples; i++)
+        {
+            string name = file_name_list[i];
+            cv::Mat im = imread(name,cv::IMREAD_GRAYSCALE);
+            Eigen::MatrixXd mat;
+            cv::cv2eigen(im, mat);
+            mat /= 255.;
+            original_data.col(i) = mat.reshaped();
+//            cout << filesystem::path(name).filename().generic_string().substr(0,5) << endl;
+            identities(i) = stoi(filesystem::path(name).filename().generic_string().substr(0,5));
+        }
+        cout << "Original Data matrix: " << original_data.rows() << " " << original_data.cols() << endl;
+//        save_vector_as_image(original_data.col(0), num_rows, num_cols, "first1.pgm");
+//        view_vector_as_image(original_data.col(0), num_rows, num_cols);
+
+        //subsetting
+        int num_identities_in_b = 0;
+        int index_of_first = 0;
+        for (int i = 0; i < num_samples; i++)
+        {
+            if (identities(i) > 50)
+            {
+                num_identities_in_b++;
+            }
+            else
+            {
+                index_of_first++;
+            }
+        }
+        cout << "Num identities in fA_b " << num_identities_in_b << endl;
+
+        Eigen::MatrixXd subset_training_data = original_data(Eigen::all, Eigen::seq(index_of_first, num_samples-1));
+        cout << "SUBSET SIZE: " << subset_training_data.rows() << " " << subset_training_data.cols() << endl;
+
+        //Sample Mean
+        //step 1
+        Eigen::VectorXd x_bar = Eigen::VectorXd::Zero(num_features);
+        for (int i = 0; i < num_identities_in_b; i++)
+        {
+            x_bar += subset_training_data.col(i);
+        }
+        x_bar /= num_identities_in_b;
+
+        Eigen::MatrixXd centered_data = Eigen::MatrixXd(num_features, num_identities_in_b);
+        for (int i = 0; i < num_identities_in_b; i++)
+        {
+            centered_data.col(i) = original_data.col(i) - x_bar;
+        }
+
+        auto A_transpose_A = centered_data.transpose() * centered_data;
+        cout << "A Transpose A matrix: " << A_transpose_A.rows() << " " << A_transpose_A.cols() << endl;
+
+        //step 4 (takes like 5 min or so)
+        Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> s(A_transpose_A);
+        //they are sorted from SMALLEST TO LARGEST.ffs.
+        cout << "Eigenvals " << s.eigenvalues().reverse() << endl;
+        //4b
+        Eigen::MatrixXd reversed_eigenvectors = s.eigenvectors().rowwise().reverse();
+
+        write_eigen_matrix_to_file(s.eigenvalues().reverse(), "eigenvalues_b.mat", "eigenvalues_b");
+
+        cout << "Checkpoint!" << endl;
+
+        //num_features x num_samples
+        //this multiplication also takes a hot minute
+        Eigen::MatrixXd transformed_eigens = centered_data * reversed_eigenvectors;
+        cout << "transformed eigenfaces dims" << transformed_eigens.rows() << " " << transformed_eigens.cols() << endl;
+        //normalize to unit length
+
+        //View top 10 eigenfaces
+        //try outputting them BEFORE normalization
+        //slide 30
+//        for (int i = 0; i < 10; i++)
+//        {
+//            double f_min = transformed_eigens.col(i).minCoeff();
+//            double f_max = transformed_eigens.col(i).maxCoeff();
+//            Eigen::VectorXd col = transformed_eigens.col(i);
+//            Eigen::VectorXd y = (col - f_min *Eigen::VectorXd::Ones(col.size())) / (f_max - f_min);
+//            view_vector_as_image(y, num_rows, num_cols);
+//        }
+
+
+        for (int i = 0; i < transformed_eigens.cols(); i++)
+        {
+            transformed_eigens.col(i) = transformed_eigens.col(i) / transformed_eigens.col(i).norm();
+        }
+
+        write_eigen_matrix_to_file(transformed_eigens, "transformed_eigens_b.mat", "transformed_eigens_b");
+    }
+    //classification on the intruders set
+    if (F)
+    {
+        auto file_name_list = get_filenames("Faces_FA_FB/fa_H");
+        cv::Mat first_sample = cv::imread("Faces_FA_FB/fa_H/00001_930831_fa_a.pgm", cv::IMREAD_GRAYSCALE);
+        int num_rows = first_sample.rows;
+        int num_cols = first_sample.cols;
+        int num_features = num_rows * num_cols;
+        int num_samples = file_name_list.size();
+        // Reads in all hd faces and stores them unflattened in a vector
+        //num_features x num_samples (NxM) matrix
+        Eigen::MatrixXd original_data = Eigen::MatrixXd(num_features, num_samples);
+        Eigen::VectorXd identities = Eigen::VectorXd(num_samples);
+        for (int i = 0; i < num_samples; i++)
+        {
+            string name = file_name_list[i];
+            cv::Mat im = imread(name,cv::IMREAD_GRAYSCALE);
+            Eigen::MatrixXd mat;
+            cv::cv2eigen(im, mat);
+            mat /= 255.;
+            original_data.col(i) = mat.reshaped();
+//            cout << filesystem::path(name).filename().generic_string().substr(0,5) << endl;
+            identities(i) = stoi(filesystem::path(name).filename().generic_string().substr(0,5));
+        }
+        cout << "Original Data matrix: " << original_data.rows() << " " << original_data.cols() << endl;
+//        save_vector_as_image(original_data.col(0), num_rows, num_cols, "first1.pgm");
+//        view_vector_as_image(original_data.col(0), num_rows, num_cols);
+
+        //subsetting
+        int num_identities_in_b = 0;
+        int index_of_first = 0;
+        for (int i = 0; i < num_samples; i++)
+        {
+            if (identities(i) > 50)
+            {
+                num_identities_in_b++;
+            }
+            else
+            {
+                index_of_first++;
+            }
+        }
+        cout << "Num identities in fA_b " << num_identities_in_b << endl;
+
+        Eigen::MatrixXd subset_training_data = original_data(Eigen::all, Eigen::seq(index_of_first, num_samples-1));
+        cout << "SUBSET SIZE: " << subset_training_data.rows() << " " << subset_training_data.cols() << endl;
+
+        //Sample Mean
+        //step 1
+        Eigen::VectorXd x_bar = Eigen::VectorXd::Zero(num_features);
+        for (int i = 0; i < num_identities_in_b; i++)
+        {
+            x_bar += subset_training_data.col(i);
+        }
+        x_bar /= num_identities_in_b;
+
+        Eigen::MatrixXd centered_data = Eigen::MatrixXd(num_features, num_identities_in_b);
+        for (int i = 0; i < num_identities_in_b; i++)
+        {
+            centered_data.col(i) = original_data.col(i) - x_bar;
+        }
+        Eigen::VectorXd eigenvalues = read_matrix_into_eigen("eigenvalues_b.mat", "eigenvalues_b");
+//        cout << eigenvalues << endl;
+        cout << eigenvalues.sum() << endl;
+//        cout << eigenvalues(0) / eigenvalues.sum() << endl;
+        //variance explained
+        Eigen::VectorXd varExp = eigenvalues / eigenvalues.sum();
+        cout << varExp(0) << endl;
+        cout << varExp(1) << endl;
+        //cumulative variance explained
+        Eigen::VectorXd cumVar(varExp.size());
+        double val = 0;
+        for (int i = 0; i < varExp.size(); i++)
+        {
+            val += varExp(i);
+            cumVar(i) = val;
+        }
+        cout << cumVar(varExp.size() - 2) << endl;
+        cout << cumVar(varExp.size() - 1) << endl;
+
+        int num_cmp_needed = -1;
+        for (int i = 0; i < cumVar.size(); i++)
+        {
+            if (cumVar(i) > 0.95)
+            {
+                num_cmp_needed = i + 1;
+                break;
+            }
+        }
+        cout << "To explain 0.95, we need " << num_cmp_needed << " of " << cumVar.size() << " components.";
+
+        Eigen::MatrixXd transformed_eigens = read_matrix_into_eigen("transformed_eigens_b.mat", "transformed_eigens_b");
+
+        //project data onto the top k eigenvectors
+        Eigen::MatrixXd top_k_eigens = transformed_eigens(Eigen::all, Eigen::seq(0, num_cmp_needed-1));
+        cout << "TOP K SIZE" << top_k_eigens.rows() << " " << top_k_eigens.cols() << endl;
+
+        Eigen::MatrixXd training_data_projected = centered_data.transpose() * top_k_eigens;
+        cout << "Projected training data: " << training_data_projected.rows() << " " << training_data_projected.cols() << endl;
+
+        Eigen::VectorXd top_k_eigenvals = eigenvalues(Eigen::seq(0, num_cmp_needed-1));
+
+        //Recognition: load in test imgs
+        auto test_names = get_filenames("Faces_FA_FB/fb_H");
+        int test_samples = test_names.size();
+        Eigen::MatrixXd test_data = Eigen::MatrixXd(num_features, test_samples);
+        Eigen::VectorXd test_identities = Eigen::VectorXd(test_samples);
+        for (int i = 0; i < test_samples; i++)
+        {
+            string name = test_names[i];
+            cv::Mat im = imread(name,cv::IMREAD_GRAYSCALE);
+            Eigen::MatrixXd mat;
+            cv::cv2eigen(im, mat);
+            mat /= 255.;
+            test_data.col(i) = mat.reshaped();
+//            cout << filesystem::path(name).filename().generic_string().substr(0,5) << endl;
+            test_identities(i) = stoi(filesystem::path(name).filename().generic_string().substr(0,5));
+        }
+        cout << "TEST DATA " << test_data.rows() << " " << test_data.cols() << endl;
+        Eigen::MatrixXd centered_test_data = Eigen::MatrixXd(num_features, test_samples);
+        for (int i = 0; i < test_samples; i++)
+        {
+            centered_test_data.col(i) = test_data.col(i) - x_bar;
+        }
+        Eigen::MatrixXd test_data_projected = centered_test_data.transpose() * top_k_eigens;
+        cout << "Projected test data: " << test_data_projected.rows() << " " << test_data_projected.cols() << endl;
+
+        //matrix of all mahalanobis dists
+        Eigen::MatrixXd training_test_difs(num_identities_in_b, test_samples);
+        int did_good = 0;
+        for (int i = 0; i < test_samples; i++)
+        {
+            for (int j = 0; j < num_identities_in_b; j++)
+            {
+                training_test_difs(j, i) = mahalanobis(training_data_projected.row(j),
+                                                       test_data_projected.row(i),
+                                                       top_k_eigenvals);
+            }
+            Eigen::VectorXd::Index index;
+            training_test_difs(Eigen::all, i).minCoeff(&index);
+//            cout << "Test Identity: " << test_identities(i) << ", Training Match: " << identities(index) << endl;
+            if (test_identities(i) == identities(index))
+            {
+                did_good++;
+            }
+        }
+        cout << "ACC: " << ((float)did_good) / ((float)test_samples) << endl;
+
+        double threshold = 0.69;
+        //info for the range of thresholds
+        cout << "Average DIFS " << training_test_difs.mean() << ", Min DIFS " << training_test_difs.minCoeff() <<
+        ", Max DIFS " << training_test_difs.maxCoeff() << endl;
     }
 
     return 0;
